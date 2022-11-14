dfae725cd2b0e3db59b10a5599649ebeda743d96
Timothy Losper <tim_losper@yahoo.com>
2022-11-14 08:43:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,15 +53,15 @@ class App extends Component {
     return (
       <div className="App">
         <h1>The Meet App</h1>
-        <h3 className="subtitle">Search for your city and see upcoming events:</h3>
+        <h3 className='subtitle'>Search for your city and see upcoming events:</h3>
         <CitySearch locations={this.state.locations}
           updateEvents={this.updateEvents} />
-        <h3>Number of shown events</h3>
+        <h3 className='subtitle'>Number of shown events</h3>
         <div>
           <NumberOfEvents numberOfEvents={this.state.numberOfEvents}
             updateNumberOfEvents={(numberOfEvents) => this.updateNumberOfEvents(numberOfEvents)} />
         </div>
-        <h3>Event List</h3>
+        <h3 className='subtitle'>Event List</h3>
         <EventList events={this.state.events} />
       </div>
     );
