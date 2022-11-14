6b0331bc2f0cc734b2ba375b5121ba4dff907e12
Timothy Losper <tim_losper@yahoo.com>
2022-11-14 08:53:20 +0100

    Update NumberofEvents.js

--- src/NumberofEvents.js
+++ src/NumberofEvents.js
@@ -38,39 +38,3 @@ class NumberOfEvents extends Component {
 export default NumberOfEvents;
 
 
-//   state = {
-//     num: 32
-//   };
-
-//   changeNum = (value) => {
-//     this.setState({ num: value });
-//     this.props.updateNumberOfEvents(value);
-//     if (value < 1 || value > 32) {
-//       this.setState({ errorText: "Select number from 1 to 32" });
-//     };
-//   };
-
-//   componentDidMount() {
-//     this.setState({ num: this.props.num || 32 });
-//   }
-
-//   render() {
-//     const { num } = this.state;
-
-//     return (
-//       <div>
-//         <label>
-//           Number of events
-//           <input
-//             className="num"
-//             type="number"
-//             value={num}
-//             onChange={(event) => this.changeNum(event.target.value)}
-//           ></input>
-//         </label>
-//       </div>
-//     );
-//   }
-// }
-
-// export default NumberOfEvents; 
