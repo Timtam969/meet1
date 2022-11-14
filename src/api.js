878ac5558ceb9fcf3dc01462f4a5e38ebad50724
Timothy Losper <tim_losper@yahoo.com>
2022-11-14 09:59:45 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -50,7 +50,7 @@ const removeQuery = () => {
 export const getEvents = async () => {
   NProgress.start();
 
-  if (window.location.href.startsWith("http://localhost")) {
+  if (window.location.href.startsWith("https://localhost")) {
     NProgress.done();
     return mockData;
   }
