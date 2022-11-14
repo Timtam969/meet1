f32e65b05f9ad4d3a6e3c90ade913a33a99f3b85
Timothy Losper <tim_losper@yahoo.com>
2022-11-14 08:40:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,33 +34,6 @@ class App extends Component {
     });
   };
 
-
-  //updateEvents = (location, maximumEvents) => {
-  // if (maximumEvents === undefined) {
-  //   maximumEvents = this.state.numberOfEvents;
-  // } else (
-  //   this.setstate({ numberOfEvents: maximumEvents })
-  // )
-  // if (location === undefined) {
-  //   location = this.state.locationSelected;
-  // }
-
-  //   getEvents().then((events) => {
-  //     const locationEvents = (location === 'all') ?
-  //       events :
-  //       events.filter((event) => event.location === location);
-  //     this.setState({
-  //       events: locationEvents.slice(0, this.state.numberOfEvents),
-  //     });
-  //   });
-  // }
-
-  // updateNumberOfEvents(number) {
-  //   this.setState({
-  //     numberOfEvents: number,
-  //   });
-  // }
-
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
